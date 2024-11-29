cff56ec3b3063f0c110cffeacf3f40e4d5fc0df8
miku39 <1716406895@qq.com>
2024-11-29 16:51:47 -0600

    upload

---
+++ omok/omok.py
@@ -0,0 +1,15 @@
+from omok.ai.ai import AI
+from omok.core.board import Board
+from omok.gui.gui import GUI
+
+def run():
+    boardwidth = 15
+    boardheight = 15
+    board = Board(width=boardwidth, height=boardheight)
+    ai = AI(board)
+    # ai.load(board.WHITE_TURN, 'minmax')
+    # ai.load(board.BLACK_TURN, 'minmax')
+    ai.start()
+    GUI(board)
+    ai.stop()
+    quit(0)
