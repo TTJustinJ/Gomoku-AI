a5fc3fa1d9e4a1c7eacaaecd5d377b6fc4962b18
mikuzhou <95454724+mikuzhou@users.noreply.github.com>
2024-11-30 20:40:59 -0600

    Update omok.py

--- omok/omok.py
+++ omok/omok.py
@@ -1,16 +1,26 @@
 from omok.ai.ai import AI
 from omok.core.board import Board
 from omok.gui.gui import GUI
+import omok.time_arrays
+import argparse
 
-def run():
+
+def run(AI_black, AI_white):
     boardwidth = 15
     boardheight = 15
     board = Board(width=boardwidth, height=boardheight)
     ai = AI(board)
-    ai.load(board.BLACK_TURN, 'randomwalk')
-    ai.load(board.WHITE_TURN, 'genetic')
-    # ai.load(board.BLACK_TURN, 'minmax')
+    ai.load(board.WHITE_TURN, AI_white)
+    ai.load(board.BLACK_TURN, AI_black)
     ai.start()
     GUI(board)
     ai.stop()
+    if AI_white == "minmax":
+        print("minmax avg decision time:" + str(omok.time_arrays.calculate_minmax_avg()))
+    elif AI_white == "randomwalk":
+        print("randomwalk avg decision time:" + str(omok.time_arrays.calculate_randomwalk_avg()))
+    if AI_black == "minmax":
+        print("minmax avg decision time:" + str(omok.time_arrays.calculate_minmax_avg()))
+    elif AI_black == "randomwalk":
+        print("randomwalk avg decision time:" + str(omok.time_arrays.calculate_randomwalk_avg()))
     quit(0)
